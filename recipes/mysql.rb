be6c6443c0676a4c296905462766bd33bde8ed02
Adam Krone <krone.adam@gmail.com>
2014-07-23 15:52:26 -0500

    Updates root password and imports db

--- recipes/mysql.rb
+++ recipes/mysql.rb
@@ -18,6 +18,12 @@ end
 package 'mysql-server'
 package 'php5-mysql'
 
+execute 'change-mysql-root-password' do
+  command "mysqladmin -u root password #{node['mysql']['server_root_password']} && touch /var/lib/mysql/password_updated"
+  creates '/var/lib/mysql/password_updated'
+  action :run
+end
+
 mysql_connection_info = {
   :host     => 'localhost',
   :username => 'root',
@@ -40,4 +46,23 @@ mysql_database_user node['codeigniter']['db']['user'] do
   database_name node['codeigniter']['db']['name']
   privileges [:all]
   action :grant
+end
+
+if node['codeigniter']['db']['backup']
+  remote_file "#{Chef::Config[:file_cache_path]}/backup.sql.tar.gz" do
+    source node['codeigniter']['db']['backup']
+    mode "0644"
+  end
+
+  execute "untar-mysql-backup" do
+    cwd Chef::Config[:file_cache_path]
+    command "tar -xzf #{Chef::Config[:file_cache_path]}/backup.sql.tar.gz"
+    creates "#{Chef::Config[:file_cache_path]}/backup.sql"
+    action :run
+  end
+
+  execute "import-db" do
+    command "mysql --password=#{node['mysql']['server_root_password']} < #{Chef::Config[:file_cache_path]}/backup.sql"
+    action :run
+  end
 end
