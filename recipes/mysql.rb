9e91af3fa040b59fb87edf567787b9d18f3d8907
Adam Krone <krone.adam@gmail.com>
2014-07-23 14:41:43 -0500

    Adds database creation

--- recipes/mysql.rb
+++ recipes/mysql.rb
@@ -17,3 +17,27 @@ end
 
 package 'mysql-server'
 package 'php5-mysql'
+
+mysql_connection_info = {
+  :host     => 'localhost',
+  :username => 'root',
+  :password => node['mysql']['server_root_password']
+}
+
+mysql_database_user node['codeigniter']['db']['user'] do
+  connection mysql_connection_info
+  password node['codeigniter']['db']['user_password']
+  action :create
+end
+
+mysql_database node['codeigniter']['db']['name'] do
+  connection mysql_connection_info
+  action :create
+end
+
+mysql_database_user node['codeigniter']['db']['user'] do
+  connection mysql_connection_info
+  database_name node['codeigniter']['db']['name']
+  privileges [:all]
+  action :grant
+end
