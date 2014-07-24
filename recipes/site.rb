cfbdf8f966f8d28bfa7491c0eb83e4c08296e8ad
Adam Krone <krone.adam@gmail.com>
2014-07-24 13:08:39 -0500

    Adds static asset retrieval

--- recipes/site.rb
+++ recipes/site.rb
@@ -13,3 +13,18 @@ web_app node['codeigniter']['site_name'] do
   docroot "/var/www/#{node['codeigniter']['docroot']}"
   allow_override 'All'
 end
+
+if node['codeigniter']['static_assets']
+  node['codeigniter']['static_assets'].each do |asset|
+    remote_file "#{Chef::Config[:file_cache_path]}/#{asset['filename']}" do
+      source asset['url']
+      mode "0644"
+    end
+
+    execute "untar-static-asset-#{asset['filename']}" do
+      command "tar -xzf #{Chef::Config[:file_cache_path]}/#{asset['filename']} -C /var/www/#{node['codeigniter']['docroot']}/static"
+      creates "/var/www/#{node['codeigniter']['docroot']}/static/#{asset['directory']}"
+      action :run
+    end
+  end
+end
